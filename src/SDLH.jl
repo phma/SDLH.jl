2c7dc5ee64a6f401d7afcce5710ecd11501eed3a
Pierre Abbat <phma@bezitopo.org>
2025-02-18 08:04:02 +0000

    Generate big primes with no special properties

--- src/SDLH.jl
+++ src/SDLH.jl
@@ -1,8 +1,8 @@
 module SDLH
-using Random,Base.Threads
-export bigRand
+using Random,Base.Threads,Primes
+export bigAnyPrime
 
-rng=RandomDevice() # temporary
+rng=RandomDevice()
 
 function bigRand(nbits::Integer)
   ret=big(0)
@@ -10,6 +10,22 @@ function bigRand(nbits::Integer)
     ret=ret<<8+rand(rng,UInt8)
   end
   ret
+end
+
+function arithProgPrime(a::T,b::T) where T<:Integer
+  while !isprime(a)
+    a+=b
+  end
+  a
+end
+
+function bigAnyPrime(nbits::Integer)
+  a=bigRand(nbits)
+  b=0x0000
+  while gcd(a,b)!=1
+    b=rand(rng,UInt16)
+  end
+  arithProgPrime(a,big(b))
 end
 
 end # module SDLH
